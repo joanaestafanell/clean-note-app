a96b972f0e3a545e71b79b8b2e88ca5a614b95b0
joana <joana.estafanell@gmail.com>
2022-03-18 09:28:26 +0100

    Use cases implemented

---
+++ app/src/main/java/com/joana/cleannoteapp/feature_note/domain/use_case/GetNoteUseCase.kt
@@ -0,0 +1,11 @@
+package com.joana.cleannoteapp.feature_note.domain.use_case
+
+import com.joana.cleannoteapp.feature_note.domain.model.Note
+import com.joana.cleannoteapp.feature_note.domain.repository.NoteRepository
+
+class GetNoteUseCase(
+    private val repository: NoteRepository
+) {
+
+    suspend operator fun invoke(id: Int): Note? = repository.getNoteById(id)
+}
